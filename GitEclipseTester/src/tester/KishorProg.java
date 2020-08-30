3adcfe29817661cf7cf687cca7a69a44a213bb6a
Kishor Kumar <kishorgn@gmail.com>
2020-08-30 13:17:26 +0530

    After adding line 2

--- GitEclipseTester/src/tester/KishorProg.java
+++ GitEclipseTester/src/tester/KishorProg.java
@@ -5,6 +5,7 @@ public class KishorProg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Kishor Program");
+		System.out.println("This is second line");
 
 	}
 
