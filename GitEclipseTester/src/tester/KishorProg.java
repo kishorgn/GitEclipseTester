294a67c55cfbc9f31751a672200ee70fbf7000ec
Kishor Kumar <kishorgn@gmail.com>
2020-08-30 13:17:01 +0530

    Git Tester - after KishorProg adding with 1 line code

---
+++ GitEclipseTester/src/tester/KishorProg.java
@@ -0,0 +1,11 @@
+package tester;
+
+public class KishorProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Kishor Program");
+
+	}
+
+}
